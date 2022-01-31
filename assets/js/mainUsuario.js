ec4f01d8a5930df380cc60de3e9942114457ab06
jmg0013 <jmg0013@alu.medac.es>
2022-01-31 11:21:40 +0100

    Practica2ParteObligatoria

--- assets/js/mainUsuario.js
+++ assets/js/mainUsuario.js
@@ -1,23 +1,48 @@
 window.onload = function () {
     fetch('http://localhost:3000/', {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-             'Accept': 'application/json',
-             'Content-Type': 'application/json'
+       method: 'GET',
+       mode: 'cors',
+       headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
         }
     })
-     .then(result => result.json())
-     .then(data => {
-         for (const clave of data) {
-             var div = document.createElement('div');
-             for (const claveInterna in clave) {
+    .then(result => result.json())
+    .then(data => {
+        for (const clave of data) {
+            var div = document.createElement('div');
+            for (const claveInterna in clave) {
                 var parrafo = document.createElement('p')
                 var nodoTexto = document.createTextNode(claveInterna+" : " + clave[claveInterna]);
                 parrafo.appendChild(nodoTexto);
                 div.appendChild(parrafo);
-             }
-             document.querySelector('#contenedor').appendChild(div);
-         }
-     })
+            }
+            document.querySelector('#contenedor').appendChild(div);
+        }
+    })
+}
+
+function buscarPorId() {
+    var input = document.querySelector("input[type='text']");
+    fetch('http://localhost:3000/usuario/?id_usuario='+ input.value, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(result => result.json())
+    .then(data => {
+        for (const clave of data) {
+            var div = document.createElement('div');
+            for (const claveInterna in clave) {
+                var parrafo = document.createElement('p')
+                var nodoTexto = document.createTextNode(claveInterna+" : " + clave[claveInterna]);
+                parrafo.appendChild(nodoTexto);
+                div.appendChild(parrafo);
+            }
+            document.querySelector('#contenedor').appendChild(div);
+        }
+    })
 }
